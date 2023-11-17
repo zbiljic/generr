ba05cc96525e3ef8a4089cb40dcbfd69975d6366
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2023-11-17 07:42:24 +0100

    chore: renovate Makefile versions

--- tools/versions.mk
+++ tools/versions.mk
@@ -1,2 +1,4 @@
+# renovate: datasource=github-releases depName=mvdan/gofumpt
 GOFUMPT_VERSION ?= v0.5.0
+# renovate: datasource=github-releases depName=golangci/golangci-lint
 GOLANGCI_LINT_VERSION ?= v1.54.2
